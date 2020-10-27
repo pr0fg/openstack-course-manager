13e6efd9f0ffddf4f65ab5aa455e9379eebf104b
Garrett Hayes (pr0fg) <garretthayes@me.com>
2020-10-27 18:46:00 -0400

    Added log file date time

--- manager/__init__.py
+++ manager/__init__.py
@@ -28,7 +28,8 @@ class OpenStackCourseManager():
 
         logging.basicConfig(level=logging.DEBUG if (debug or Config.DEBUG)
                             else logging.INFO,
-                            format=f'%(levelname)s: %(message)s',
+                            datefmt='%m-%d %H:%M',
+                            format=f'%(asctime)s %(levelname)s: %(message)s',
                             filename=Config.LOG_FILE)
 
         openstack.enable_logging(
